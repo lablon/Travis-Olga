eaf07b3381117bd440d0e445886b925d6f43b947
akostyko <34099073+akostyko@users.noreply.github.com>
2017-12-06 15:34:51 +0300

    first commit
     first comment

---
+++ src/com/ibm/minsk/test/Exercise.java
@@ -0,0 +1,8 @@
+package com.ibm.minsk.test;
+
+public class Exercise {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+    	System.out.println("Hello World!");
+    }
+}
